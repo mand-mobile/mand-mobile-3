1c4d3f502991900aafaa31c45c62e824a6d74bef
xuxiaoyan <xuxiaoyan@didiglobal.com>
2021-04-16 18:46:35 +0800

    💡 refactor(builder): refactor vue cli plugin commands with new builder

--- packages/vue-cli-plugin-builder/src/index.ts
+++ packages/vue-cli-plugin-builder/src/index.ts
@@ -1,16 +1,16 @@
-import R from 'ramda'
 import * as path from 'path'
 // import WebpackChain from 'webpack-chain/types'
 
 // import {} from '@mand-mobile/platform-builder'
 
 
-const commands = ['md-preview', 'md-install']
+// const commands = ['md-preview', 'md-install']
+const commands = ['md-preview']
 
-export = (api:any, projectOptions: any) => {
 
-  R.forEach((command) => {
+export = (api:any) => {
+  commands.forEach(command => {
     const handler = require(path.resolve(__dirname, `commands/${command}`))
     api.registerCommand(command, handler(api))
-  }, commands)
+  })
 }
